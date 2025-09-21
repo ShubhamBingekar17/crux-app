c3ea5c632cf4233db94ef40cb95a3ca1358aa6d2
Shubham.bingekar <shubham.bingekar@bdiplus.com>
2025-09-21 20:53:31 +0530

    FIX: package missing

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
+import axios from "axios";
+
 export const getCruxData = async (urls, deviceType) => {
   try {
-    const response = await axios.post("/api/crux", {url , device: deviceType });
+    const response = await axios.post("/api/crux", {urls , device: deviceType });
     return response.data.results;
   } catch (e) {
     return [{ success: false, error: e.toString() }];
