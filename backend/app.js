dda0d2a5771f11321545db47a3a44068bd78bf26
Shubham Bingekar <75237471+ShubhamBingekar17@users.noreply.github.com>
2025-09-21 22:14:28 +0530

    ADD: cors

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
 const cruxRoutes = require("./routes/cruxRoutes");
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
