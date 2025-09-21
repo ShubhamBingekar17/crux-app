aa2473227aaf5602c27a706286b46bec92ce8c0a
Shubham Bingekar <75237471+ShubhamBingekar17@users.noreply.github.com>
2025-09-21 22:13:00 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getCruxData = async (urls, deviceType) => {
   try {
-    const response = await axios.post("/api/crux", {urls , device: deviceType });
+    const response = await axios.post("https://crux-backend-nine.vercel.app/api/crux", {urls , device: deviceType });
     return response.data.results;
   } catch (e) {
     return [{ success: false, error: e.toString() }];
